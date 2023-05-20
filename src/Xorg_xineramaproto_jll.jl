83295a12ba1202165553b3978d11d9289013981e
jlbuild <juliabuildbot@gmail.com>
2023-05-20 16:50:50 +0000

    Xorg_xineramaproto_jll build 1.2.1+1

--- src/Xorg_xineramaproto_jll.jl
+++ src/Xorg_xineramaproto_jll.jl
@@ -1,31 +1,9 @@
-module Xorg_xineramaproto_jll
-using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
-import Base: UUID
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule Xorg_xineramaproto_jll
+using Base
+using Base: UUID
+import JLLWrappers
 
-# We put these inter-JLL-package API values here so that they are always defined, even if there
-# is no underlying wrapper held within this JLL package.
-const PATH_list = String[]
-const LIBPATH_list = String[]
-
-# Load Artifacts.toml file
-artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
-
-# Extract all platforms
-artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("6a3da44c-33b1-5374-838f-bf0fbf92c29b"))
-platforms = [Pkg.Artifacts.unpack_platform(e, "Xorg_xineramaproto", artifacts_toml) for e in artifacts["Xorg_xineramaproto"]]
-
-# Filter platforms based on what wrappers we've generated on-disk
-platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
-
-# From the available options, choose the best platform
-best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
-
-# Silently fail if there's no binaries for this platform
-if best_platform === nothing
-    @debug("Unable to load Xorg_xineramaproto; unsupported platform $(triplet(platform_key_abi()))")
-else
-    # Load the appropriate wrapper
-    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
-end
-
+JLLWrappers.@generate_main_file_header("Xorg_xineramaproto")
+JLLWrappers.@generate_main_file("Xorg_xineramaproto", UUID("6a3da44c-33b1-5374-838f-bf0fbf92c29b"))
 end  # module Xorg_xineramaproto_jll
